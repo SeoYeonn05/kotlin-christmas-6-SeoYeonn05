813f92eb2f620c0f041afc840b1e4c92011c055b
seoyeon <seoyeonn051@gmail.com>
2023-11-11 00:57:33 +0900

    fix: Constants.kt 분리 및 ErrorConstants.kt 생성

--- src/main/kotlin/christmas/util/constant/Constants.kt
+++ src/main/kotlin/christmas/util/constant/Constants.kt
@@ -1,20 +1,4 @@
-package christmas.util
-
-
-object MenuConstants{
-    const val MUSHROOM_SOUP = "양송이수프"
-    const val TAPAS = "타파스"
-    const val CAESAR_SALAD = "시저샐러드"
-    const val T_BONE_STEAK = "티본스테이크"
-    const val BBQ_RIBS = "바비큐립"
-    const val SEAFOOD_PASTA = "해산물파스타"
-    const val CHRISTMAS_PASTA = "크라스마스파스타"
-    const val CHOCOLATE_CAKE = "초코케이크"
-    const val ICE_CREAM = "아이스크림"
-    const val ZERO_COLA = "제로콜라"
-    const val RED_WINE = "레드와인"
-    const val CHAMPAGNE = "샴페인"
-}
+package christmas.util.constant
 
 object Constants {
     private const val PROMOTION_MONTH = 12
@@ -37,8 +21,3 @@ object Constants {
     const val WON = "원"
 }
 
-object BadgeConstants {
-    const val STAR ="별"
-    const val TREE ="트리"
-    const val SANTA ="산타"
-}
---
+++ src/main/kotlin/christmas/util/constant/ErrorConstants.kt
@@ -0,0 +1,6 @@
+package christmas.util.constant
+
+object ErrorConstants {
+    const val EMPTY_VALUE_ERROR = "값을 입력해주세요."
+    const val INVALID_DATE_ERROR = "유효하지 않은 날짜입니다. 다시 입력해 주세요."
+}
