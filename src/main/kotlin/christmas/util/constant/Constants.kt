8bbe7735926d6e595e711f5ef55111319f5e3674
seoyeon <seoyeonn051@gmail.com>
2023-11-11 20:43:45 +0900

    fix: Constants.kt 상수 추가

--- src/main/kotlin/christmas/util/constant/Constants.kt
+++ src/main/kotlin/christmas/util/constant/Constants.kt
@@ -16,6 +16,7 @@ object Constants {
     const val DECEMBER_EVENT_BADGE = "<12월 이벤트 배지>"
     const val CHRISTMAS_D_DAY_DISCOUNT = "크리스마스 디데이 할인:"
     const val WEEKDAY_DISCOUNT = "평일 할인:"
+    const val WEEKEND_DISCOUNT = "주말 할인:"
     const val SPECIAL_DISCOUNT = "특별 할인:"
     const val GIVEAWAY_PROMOTION = "증정 이벤트:"
     const val WON = "원"
