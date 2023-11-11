afe4db03e540aeb54acac69010e6386737cd1e15
seoyeon <seoyeonn051@gmail.com>
2023-11-11 20:44:46 +0900

    feat: PromotionDate.kt 생성

---
+++ src/main/kotlin/christmas/model/PromotionDate.kt
@@ -0,0 +1,69 @@
+package christmas.model
+
+import christmas.util.DayOfWeekChecker
+
+enum class DayOfWeekType(val dayOfWeekNum: Int) {
+    SUNDAY(dayOfWeekNum = 1),
+    MONDAY(dayOfWeekNum = 2),
+    TUESDAY(dayOfWeekNum = 3),
+    WEDNESDAY(dayOfWeekNum = 4),
+    THURSDAY(dayOfWeekNum = 5),
+    FRIDAY(dayOfWeekNum = 6),
+    SATURDAY(dayOfWeekNum = 7),
+}
+
+enum class DiscountPromotion(private val promotionRange: List<Int>) {
+    CHRISTMAS_D_DAY_DISCOUNT_PROMOTION((1..25).toList()),
+    WEEKDAY_DISCOUNT_PROMOTION((DayOfWeekType.SUNDAY.dayOfWeekNum..DayOfWeekType.THURSDAY.dayOfWeekNum).toList()),
+    WEEKEND_DISCOUNT_PROMOTION((DayOfWeekType.FRIDAY.dayOfWeekNum..DayOfWeekType.SATURDAY.dayOfWeekNum).toList()),
+    SPECIAL_DISCOUNT_PROMOTION(listOf(DayOfWeekType.SUNDAY.dayOfWeekNum, 25)),
+    NO_PROMOTION(listOf(0));
+
+    fun checkPromotionRange(date: Int): Boolean {
+        return date in promotionRange
+    }
+}
+
+class PromotionDate(private val reservationDate: Int) {
+    private lateinit var availablePromotions: MutableMap<DiscountPromotion, Boolean>
+    private val dayOfWeek = DayOfWeekChecker()
+
+    init {
+        checkPromotionDate()
+    }
+
+    fun getAvailablePromotions(): MutableMap<DiscountPromotion, Boolean> {
+        return availablePromotions
+    }
+
+    private fun checkPromotionDate() {
+        availablePromotions[DiscountPromotion.CHRISTMAS_D_DAY_DISCOUNT_PROMOTION] =
+            isChristmasDDayPromotionDate(reservationDate)
+        availablePromotions[DiscountPromotion.WEEKDAY_DISCOUNT_PROMOTION] =
+            isWeekdayPromotionDate(reservationDate)
+        availablePromotions[DiscountPromotion.WEEKEND_DISCOUNT_PROMOTION] =
+            isWeekendPromotionDate(reservationDate)
+        availablePromotions[DiscountPromotion.SPECIAL_DISCOUNT_PROMOTION] =
+            isSpecialPromotionDate(reservationDate)
+    }
+
+    private fun isChristmasDDayPromotionDate(date: Int): Boolean {
+        var dayOfWeek = dayOfWeek.doDayOfWeek(day = date)
+        return DiscountPromotion.CHRISTMAS_D_DAY_DISCOUNT_PROMOTION.checkPromotionRange(dayOfWeek)
+    }
+
+    private fun isWeekdayPromotionDate(date: Int): Boolean {
+        var dayOfWeek = dayOfWeek.doDayOfWeek(day = date)
+        return DiscountPromotion.WEEKDAY_DISCOUNT_PROMOTION.checkPromotionRange(dayOfWeek)
+    }
+
+    private fun isWeekendPromotionDate(date: Int): Boolean {
+        var dayOfWeek = dayOfWeek.doDayOfWeek(day = date)
+        return DiscountPromotion.WEEKEND_DISCOUNT_PROMOTION.checkPromotionRange(dayOfWeek)
+    }
+
+    private fun isSpecialPromotionDate(date: Int): Boolean {
+        var dayOfWeek = dayOfWeek.doDayOfWeek(day = date)
+        return DiscountPromotion.SPECIAL_DISCOUNT_PROMOTION.checkPromotionRange(dayOfWeek)
+    }
+}
