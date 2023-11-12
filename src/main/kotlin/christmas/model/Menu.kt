5c69cc6f3b1d5b278971b1f723e2bdfc08f1fbd5
seoyeon <seoyeonn051@gmail.com>
2023-11-12 20:32:27 +0900

    rename: Menu 이름 변경

--- src/main/kotlin/christmas/model/Menu.kt
+++ src/main/kotlin/christmas/model/Menu.kt
@@ -1,6 +1,6 @@
 package christmas.model
 
-class Menu(private val menuItem: MenuItem, private val orderedMenuCount: Int){
+class MenuOrder(private val menuItem: MenuItem, private val orderedMenuCount: Int){
     init {
         //예외처리
     }
