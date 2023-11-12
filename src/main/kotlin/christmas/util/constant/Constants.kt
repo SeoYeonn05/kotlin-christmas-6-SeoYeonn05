bd179ea4c5003192f3d028123d8c43c74c32c1cd
seoyeon <seoyeonn051@gmail.com>
2023-11-12 19:02:44 +0900

    fix: getPromotionBenefitsPreviewMessage 함수 생성

--- src/main/kotlin/christmas/util/constant/Constants.kt
+++ src/main/kotlin/christmas/util/constant/Constants.kt
@@ -3,11 +3,11 @@ package christmas.util.constant
 object Constants {
     const val PROMOTION_YEAR = 2023
     const val PROMOTION_MONTH = 12
-    private const val promotionDate = 3
+    private var promotionDate = 0
     const val START_MESSAGE = "안녕하세요! 우테코 식당 ${PROMOTION_MONTH}월 이벤트 플래너입니다."
     const val REQUEST_RESERVATION_DATE = "${PROMOTION_MONTH}월 중 식당 예약 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
     const val REQUEST_RESERVATION_MENU = "주문하실 메뉴를 메뉴와 개수로 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
-    const val INFORM_PROMOTION_BENEFITS = "${PROMOTION_MONTH}월 ${promotionDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
+    var PROMOTION_BENEFITS_PREVIEW = "${PROMOTION_MONTH}월 ${promotionDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
     const val ORDERED_MENU = "<주문 메뉴>"
     const val TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT = "<할인 전 총주문 금액>"
     const val GIVEAWAY_MENU = "<증정 메뉴>"
@@ -21,5 +21,10 @@ object Constants {
     const val SPECIAL_DISCOUNT = "특별 할인:"
     const val GIVEAWAY_PROMOTION = "증정 이벤트:"
     const val WON = "원"
+
+    fun getPromotionBenefitsPreviewMessage(date: Int): String {
+        promotionDate = date
+        return PROMOTION_BENEFITS_PREVIEW
+    }
 }
 
