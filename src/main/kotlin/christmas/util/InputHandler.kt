f6cc80dfb5e90e09ec68547de71e29cd2d330829
seoyeon <seoyeonn051@gmail.com>
2023-11-12 23:47:45 +0900

    fix: inputHandler 함수에 validateInput 매개변수 추가 및 적용

--- src/main/kotlin/christmas/util/InputHandler.kt
+++ src/main/kotlin/christmas/util/InputHandler.kt
@@ -1,9 +1,28 @@
 package christmas.util
 
 import camp.nextstep.edu.missionutils.Console
+import christmas.validation.exception.EmptyFieldException
+import christmas.validation.exception.IllegalDateException
 
 class InputHandler {
-    fun inputHandler():String {
-        return Console.readLine()
+    fun inputHandler(validateInput: (String) -> Boolean): String {
+        var input: String
+        do {
+            input = Console.readLine()
+            if (input.isBlank()) {
+                EmptyFieldException.emptyField
+            }
+        } while (!validateInput(input))
+        return input
+    }
+
+    fun example() {
+        inputHandler { input ->
+            val isValid = input.all { it.isLetter() }
+            if(!isValid){
+                IllegalDateException
+            }
+            isValid
+        }
     }
 }
