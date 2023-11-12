39fc4ec499122afaa320982d8820b53c0660b781
seoyeon <seoyeonn051@gmail.com>
2023-11-13 00:30:57 +0900

    fix: OutputView.kt 함수 출력 기능 추가

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -1,38 +1,50 @@
 package christmas.view
 
 import christmas.model.MenuOrder
+import christmas.util.DataFormatter
+import christmas.util.constant.Constants
+import java.text.Format
 
 class OutputView {
     private val print = Printer()
+    private val format = DataFormatter()
 
     fun printPreviewPromotionMessage(){
         print.printPreviewPromotionMessage()
     }
     fun printOrderedMenu(orderedMenu: List<MenuOrder>){
         print.printOrderedMenuMessage()
-
+        orderedMenu.forEach {
+            println("${it.printMenuItem()} ${it.printOrderedMenuCount()}${Constants.UNIT}")
+        }
     }
     fun printTotalOrderAmountBeforeDiscount(totalOrderedAmountBeforeDiscount: Int){
         print.printTotalOrderAmountBeforeDiscountMessage()
+        if(totalOrderedAmountBeforeDiscount == 0){
+            println(Constants.NO_VALUE)
+        }
+        val formatTotalOrderAmount = format.formatNumberToDecimal(totalOrderedAmountBeforeDiscount)
+        println("$formatTotalOrderAmount$Constants.WON")
 
     }
     fun printGiveaway(/**/){
         print.printGiveawayMenuMessage()
 
     }
-    fun print(){
+    fun printBenefitDetailsMessage(){
         print.printBenefitDetailsMessage()
-
     }
     fun printTotalBenefitAmount(totalBenefitAmount: Int){
         print.printTotalBenefitAmountMessage()
-
+        val formatTotalBenefitAmount = format.formatNumberToDecimal(totalBenefitAmount)
+        println("$formatTotalBenefitAmount$Constants.WON")
     }
     fun printExpectedPayment(expectedPayment: Int){
         print.printExpectedPaymentAfterDiscountMessage()
-
+        val formatExpectedPayment = format.formatNumberToDecimal(expectedPayment)
+        println("$formatExpectedPayment$Constants.WON")
     }
-    fun printDecemberEventBadge(totalBenefitAmount: Int){
+    fun printDecemberEventBadge(eventBadge: String){
         print.printDecemberEventBadgeMessage()
 
     }
