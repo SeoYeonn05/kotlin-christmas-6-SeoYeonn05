f1b0678aeae52e3b894fd5dc270e559d85e44913
seoyeon <seoyeonn051@gmail.com>
2023-11-13 00:30:34 +0900

    fix: printMenuItem, printOrderedMenuCount 함수 추가

--- src/main/kotlin/christmas/model/Menu.kt
+++ src/main/kotlin/christmas/model/Menu.kt
@@ -13,6 +13,14 @@ class MenuOrder(private val menuItem: MenuItem, private val orderedMenuCount: In
         return totalMenuCount + orderedMenuCount
     }
 
+    fun printMenuItem(){
+        print(menuItem)
+    }
+
+    fun printOrderedMenuCount(){
+        print(orderedMenuCount)
+    }
+
     companion object {
         private const val MIN_MENU_COUNT = 1
         private const val MAX_MENU_COUNT = 20
