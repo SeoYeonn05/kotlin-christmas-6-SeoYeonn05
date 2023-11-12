3f44e2e33a227fef7549cb04b9397222510c3477
seoyeon <seoyeonn051@gmail.com>
2023-11-13 00:05:39 +0900

    fix: inputHandler 수정에 따라 InputProcessor 함수 수정

--- src/main/kotlin/christmas/view/InputProcessor.kt
+++ src/main/kotlin/christmas/view/InputProcessor.kt
@@ -1,32 +1,29 @@
 package christmas.view
 
 import christmas.util.InputHandler
-import christmas.util.constant.Constants
 import christmas.validation.InputValidator
+import christmas.validation.exception.IllegalDateException
+import christmas.validation.exception.IllegalMenuException
 
 class InputProcessor {
     fun requestReservationDate(): String {
-        lateinit var input: String
-        var validInput: Boolean = false
-
-
-        while (!validInput) {
-            input = InputHandler().inputHandler()
-            validInput = InputValidator().validateInputReservationDate(input)
-            println(input)
+        var input: String = InputHandler().inputHandler { input ->
+            val isValid = InputValidator().validateInputReservationDate(input)
+            if (!isValid) {
+                IllegalDateException.invalidNumericValue
+            }
+            isValid
         }
         return input
     }
 
     fun requestReservationMenu(): String {
-        lateinit var input: String
-        var validInput: Boolean = false
-        print(Constants.REQUEST_RESERVATION_DATE)
-
-        while (!validInput) {
-            input = InputHandler().inputHandler()
-            validInput = InputValidator().validateInputReservationMenu(input)
-            println(input)
+        var input: String = InputHandler().inputHandler { input ->
+            val isValid = InputValidator().validateInputReservationMenu(input)
+            if (!isValid) {
+                IllegalMenuException.invalidMenuFormat
+            }
+            isValid
         }
         return input
     }
