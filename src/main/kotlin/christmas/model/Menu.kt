ab0bd7e9951b33ce635597ec1136c01e3eb3a58f
seoyeon <seoyeonn051@gmail.com>
2023-11-12 23:48:17 +0900

    fix: validate 메뉴 함수 추가

--- src/main/kotlin/christmas/model/Menu.kt
+++ src/main/kotlin/christmas/model/Menu.kt
@@ -1,7 +1,20 @@
 package christmas.model
 
-class MenuOrder(private val menuItem: MenuItem, private val orderedMenuCount: Int){
+class MenuOrder(private val menuItem: MenuItem, private val orderedMenuCount: Int) {
     init {
         //예외처리
+    }
+
+    fun validateOrderMenu(): Boolean {
+        return menuItem != MenuItem.NO_MENU
+    }
+
+    fun validateMenuRange(): Boolean {
+        return orderedMenuCount >= MIN_MENU_COUNT
+    }
+
+    companion object {
+        private const val MIN_MENU_COUNT = 1
+        private const val MAX_MENU_COUNT = 20
     }
 }
