299158c0a935901653b7651f4c1f3f7d7c0c4feb
seoyeon <seoyeonn051@gmail.com>
2023-11-13 00:00:41 +0900

    fix: validateTotalMenuCount 함수 추가

--- src/main/kotlin/christmas/model/MenuManager.kt
+++ src/main/kotlin/christmas/model/MenuManager.kt
@@ -1,12 +1,15 @@
 package christmas.model
 
 import christmas.util.DataFormatter
+import christmas.validation.exception.IllegalMenuException
 
 class MenuManager(private val inputReservationMenu: String) {
     private lateinit var orderedMenu: MutableList<MenuOrder>
 
     init {
         createOrderedMenu()
+        //MenuInvalidator(orderedMenu)
+        //validateTotalMenuCount()
     }
 
     private fun createOrderedMenu() {
@@ -16,11 +19,25 @@ class MenuManager(private val inputReservationMenu: String) {
         }
     }
 
-    private fun convertReservationMenu(menuStr: String):MenuOrder{
+    private fun convertReservationMenu(menuStr: String): MenuOrder {
         val splitMenuString = menuStr.split("-")
         val menuName = MenuItem.isMenuAvailable(splitMenuString[0])
         val menuCount = DataFormatter().parseToInt(splitMenuString[1])
 
-        return MenuOrder(menuName ,menuCount)
+        return MenuOrder(menuName, menuCount)
+    }
+
+    private fun validateTotalMenuCount() {
+        var totalMenuCount = 0
+        orderedMenu.forEach { menuOrder ->
+            totalMenuCount = menuOrder.sumMenuCount(totalMenuCount)
+        }
+        if (totalMenuCount > MAX_TOTAL_MENU_COUNT) {
+            IllegalMenuException.invalidMenuCount
+        }
+    }
+
+    companion object {
+        private const val MAX_TOTAL_MENU_COUNT = 20
     }
 }
