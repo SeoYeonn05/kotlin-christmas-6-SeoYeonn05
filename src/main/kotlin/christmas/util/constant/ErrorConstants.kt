bb51a4e82c55c3af20b6962c34826bc9815af6c6
seoyeon <seoyeonn051@gmail.com>
2023-11-14 12:05:48 +0900

    fix: Error 메시지 추가

--- src/main/kotlin/christmas/util/constant/ErrorConstants.kt
+++ src/main/kotlin/christmas/util/constant/ErrorConstants.kt
@@ -5,4 +5,6 @@ object ErrorConstants {
     const val EMPTY_VALUE_ERROR = "값을 입력해주세요."
     const val INVALID_DATE_ERROR = "유효하지 않은 날짜입니다. 다시 입력해 주세요."
     const val INVALID_MENU_ERROR = "유효하지 않은 주문입니다. 다시 입력해 주세요."
+    const val INVALID_PROMOTION_ERROR = "종료 되었거나 존재하지 않는 이벤트 입니다. 시스템을 다시 시작해주세요."
+    const val LIMITED_PROMOTION_ERROR = "이벤트 대상이 아닙니다. 주문을 진행합니다."
 }
