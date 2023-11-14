405e0a7d1c4e0cc8feb4e4a5bae682264521cf02
seoyeon <seoyeonn051@gmail.com>
2023-11-14 18:16:32 +0900

    fix: DiscountCalculator.kt 클래스 기능 변경
    calculateDiscountAmount 함수 생성

--- src/main/kotlin/christmas/model/DiscountCalculator.kt
+++ src/main/kotlin/christmas/model/DiscountCalculator.kt
@@ -2,23 +2,11 @@ package christmas.model
 
 import christmas.validation.exception.IllegalPromotionException
 
-class DiscountCalculator(
-    private val availablePromotion: DiscountPromotion,
-    private val orderedMenu: MenuOrder?
-) {
-    init {
-        when (availablePromotion) {
-            DiscountPromotion.CHRISTMAS_D_DAY_DISCOUNT_PROMOTION -> {}
-            DiscountPromotion.WEEKDAY_DISCOUNT_PROMOTION -> applyWeekDayPromotion()
-            DiscountPromotion.WEEKEND_DISCOUNT_PROMOTION -> applyWeekendPromotion()
-            DiscountPromotion.SPECIAL_DISCOUNT_PROMOTION -> {}
-            else -> IllegalPromotionException.invalidPromotion
-        }
-    }
-    private fun applyWeekDayPromotion() {
-        orderedMenu?.calculateDiscountedAmount(availablePromotion)
-    }
-    private fun applyWeekendPromotion() {
-        orderedMenu?.calculateDiscountedAmount(availablePromotion)
+class DiscountCalculator {
+    fun calculateDiscountAmount(
+        availablePromotion: DiscountPromotion,
+        orderedMenu: MenuOrder
+    ): Int {
+        return availablePromotion.getDiscountAmount() * orderedMenu.getOrderedMenuCount()
     }
 }
