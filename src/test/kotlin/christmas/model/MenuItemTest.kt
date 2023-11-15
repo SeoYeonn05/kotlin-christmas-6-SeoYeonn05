ae446e1fbb93e2f42861b7bf4c591e938da22bea
seoyeon <seoyeonn051@gmail.com>
2023-11-15 12:08:49 +0900

    test: MenuItemTest.kt 생성

---
+++ src/test/kotlin/christmas/model/MenuItemTest.kt
@@ -0,0 +1,27 @@
+package christmas.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MenuItemTest {
+
+    @Test
+    fun `존재하는 메뉴 입력`() {
+        val expectedMenuItem = MenuItem.SEAFOOD_PASTA
+        val actualMenuItem = MenuItem.isMenuAvailable(SEA_FOOD_PASTA)
+
+        assertEquals(expectedMenuItem, actualMenuItem)
+    }
+
+    @Test
+    fun `존재하지 않는 메뉴 입력`() {
+        val expectedMenuItem = MenuItem.NO_MENU
+        val actualMenuItem = MenuItem.isMenuAvailable(COFFEE)
+
+        assertEquals(expectedMenuItem, actualMenuItem)
+    }
+    companion object{
+       const val SEA_FOOD_PASTA = "해산물파스타"
+       const val COFFEE = "커피"
+    }
+}
