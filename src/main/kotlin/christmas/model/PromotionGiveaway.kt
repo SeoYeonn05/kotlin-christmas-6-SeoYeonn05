4e77edb5ac1c1875bab804cbd18566b0b98e87c2
seoyeon <seoyeonn051@gmail.com>
2023-11-15 15:26:51 +0900

    fix: GiveawayItem giveaway 변수 추가
    get 증정품, 증정품 가격 함수 추가

--- src/main/kotlin/christmas/model/PromotionGiveaway.kt
+++ src/main/kotlin/christmas/model/PromotionGiveaway.kt
@@ -1,11 +1,17 @@
 package christmas.model
 
 enum class GiveawayItem(
-    private val amount: Int
+    private val giveaway: String,
+    private val amount: Int,
+    private val giveawayAmount: Int
 ) {
-    CHAMPAGNE(120000),
-    NO_GIVEAWAY(0);
+    CHAMPAGNE("샴페인", 120000, 25000),
+    NO_GIVEAWAY("없음",0, 0);
 
+    fun getGiveaway() = this.giveaway
+    fun applyGiveawayAMount(amount: Int):Int{
+        return amount - this.giveawayAmount
+    }
     companion object {
         fun getGiveawayByAmount(amount: Int): GiveawayItem {
             return when {
