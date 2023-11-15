975f219035583eaffd326f5caeaf2fce01ef0961
seoyeon <seoyeonn051@gmail.com>
2023-11-15 17:10:36 +0900

    fix: getPromotionHistory 함수 생성, 일부 예외처리 코드 위치 이동

--- src/main/kotlin/christmas/model/OrderProcessor.kt
+++ src/main/kotlin/christmas/model/OrderProcessor.kt
@@ -2,47 +2,45 @@ package christmas.model
 
 
 class OrderProcessor(
+    private val inputDate: String,
     private val orderedMenus: MutableList<MenuOrder>,
-    private val discountPromotion: MutableList<DiscountPromotion>
 ) {
+    private var promotionDate: PromotionDate = PromotionDate(inputDate)
     private var totalOrderPrice: Int = 0
+    private val availablePromotions: MutableList<DiscountPromotion> = promotionDate.getAvailablePromotions()
+    private val promotionApplicator: PromotionApplicator =
+        PromotionApplicator(promotionDate.getReservationDate(), availablePromotions, orderedMenus)
 
-    private fun isEventAvailable(): Boolean {
-        return checkTotalOrderPriceDiscount() && checkBeverageOnlyOrder() && checkMaximumMenuCount()
+    init{
+        calculateTotalOrderPrice()
+    }
+    fun getTotalOrderPrice() = totalOrderPrice
+
+
+    private fun calculateTotalOrderPrice(){
+        orderedMenus.forEach { menuOrder ->
+            totalOrderPrice += menuOrder.calculateMenuAmount()
+        }
+    }
+
+    fun getPromotionHistory(): MutableMap<DiscountPromotion, Int> {
+        if (checkPromotionConditions()) {
+            return promotionApplicator.applyPromotion()
+        }
+        return mutableMapOf(
+            DiscountPromotion.NO_PROMOTION to 0
+        )
+    }
+
+    private fun checkPromotionConditions(): Boolean {
+        return checkTotalOrderPriceDiscount()
     }
 
     private fun checkTotalOrderPriceDiscount(): Boolean {
         return totalOrderPrice >= MINIMUM_TOTAL_ORDER_AMOUNT
     }
 
-    private fun checkBeverageOnlyOrder(): Boolean {
-        var beverageMenus = orderedMenus.filter { menuOrder ->
-            menuOrder.validateIsBeverage()
-        }
-        return beverageMenus.size != orderedMenus.size
-    }
-
-    private fun checkMaximumMenuCount(): Boolean {
-        var totalMenuCount = 0
-        orderedMenus.forEach { menuOrder ->
-            totalMenuCount = menuOrder.sumMenuCount(totalMenuCount)
-        }
-        return totalMenuCount <= MAX_TOTAL_MENU_COUNT
-    }
-
-    fun getTotalOrderPrice() {
-
-    }
-
-    fun getDiscountAmount() {
-        orderedMenus.forEach { menuOrder ->
-            totalOrderPrice += menuOrder.calculateMenuAmount()
-        }
-    }
-
     companion object {
         private const val MINIMUM_TOTAL_ORDER_AMOUNT = 10000
-        private const val MAX_TOTAL_MENU_COUNT = 20
-
     }
 }
